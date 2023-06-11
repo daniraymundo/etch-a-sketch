9dd1ff3f7a32d70d30a387daad8e52b415e97f2c
Dani <dmmraymundo1@gmail.com>
2023-06-11 12:38:01 +0800

    Add createGrid function

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
+const resetButton = document.querySelector(".reset");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.className = "grid-square";
-    gridContainer.appendChild(square);
-};
+function createGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let gridArea = size * size
+
+    for (let i = 0; i < gridArea; i++) {
+        const square = document.createElement("div");
+        square.className = "grid-square";
+        gridContainer.appendChild(square);
+    };
+}
+
+createGrid(16);
 
 const squares = document.querySelectorAll(".grid-square");
 
 squares.forEach(square => {
     square.addEventListener("mouseover", event => {
-        square.classList.add("colored");
+        square.classList.add("colored-black");
     });
 });
