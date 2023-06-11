850687537a82c43856d42469c84fc32c7333e60c
Dani <dmmraymundo1@gmail.com>
2023-06-11 11:12:58 +0800

    Add black hover effect

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.className = "grid-square";
     gridContainer.appendChild(square);
-}
+};
+
+const squares = document.querySelectorAll(".grid-square");
+
+squares.forEach(square => {
+    square.addEventListener("mouseover", event => {
+        square.classList.add("colored");
+    });
+});
