6f4ad32b031870fed8e95940b9d52d5e45fcf608
Dani <dmmraymundo1@gmail.com>
2023-06-12 10:58:37 +0800

    Add black mode

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const gridContainer = document.querySelector(".grid-container");
 const resetButton = document.querySelector(".reset");
+const blackButton = document.querySelector(".black");
+const rainbowButton = document.querySelector(".rainbow");
 let squares = [];
+let blackModeEnabled = false;
 
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -15,25 +18,40 @@ function createGrid(size) {
     };
 
     squares = document.querySelectorAll(".grid-square");
-
-    squares.forEach(square => {
-        square.addEventListener("mouseover", event => {
-            square.classList.add("colored-black");
-        });
-    });
-}
+};
 
 createGrid(16);
 
+function enableBlackMode() {
+    blackModeEnabled = true;
+    squares.forEach(square => {
+        square.addEventListener("mouseover", event => {
+            if (blackModeEnabled) {
+                square.classList.add("colored-black");
+            };
+        });
+    });
+};
+
+blackButton.addEventListener("click", event => {
+    enableBlackMode();
+});
+
+function disableBlackMode() {
+    blackModeEnabled = false;
+    squares.forEach(square => {
+        square.classList.remove("colored-black");
+    });
+};
+
+
 resetButton.addEventListener("click", event => {
+    disableBlackMode();
     let size = prompt("Enter a number between 2 and 100. This will be the grid size.");
     while (isNaN(size) || size < 2 || size > 100) {
         size = prompt("Invalid input. Minimum size is 2x2. Maximum size is 100x100. Enter a number between 2 and 100.");
     };
-    
+
     createGrid(size);
     
-    squares.forEach(square => {
-        square.classList.remove("colored-black");
-    })
 });
