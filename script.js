b9312953bb4c1010610fb65d5bbe4672ad3f2c89
Dani <dmmraymundo1@gmail.com>
2023-06-16 11:05:16 +0800

    Refactor event handling

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const eraserButton = document.querySelector(".eraser");
 const inputBox = document.querySelector(".input-box")
 const inputMessage = document.querySelector(".input-message")
 let squares = [];
-let blackModeEnabled = false;
-let rainbowModeEnabled = false;
+let currentMode = "default";
 
 function createGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -18,92 +17,50 @@ function createGrid(size) {
     for (let i = 0; i < gridArea; i++) {
         const square = document.createElement("div");
         square.className = "grid-square";
+        square.addEventListener("mouseover",handleDraw);
         gridContainer.appendChild(square);
     };
 
     squares = document.querySelectorAll(".grid-square");
+    squares.forEach(square => square.removeAttribute("style"));
 };
 
 createGrid(16);
 
-function enableBlackMode() {
-    blackModeEnabled = true;
-    squares.forEach(square => {
-        square.addEventListener("mouseover", event => {
-            if (blackModeEnabled) {
-                square.style.backgroundColor = "black";
-            };
-        });
-    });
-};
 
-blackButton.addEventListener("click", event => {
-    enableBlackMode();
-});
+blackButton.addEventListener("click", event => currentMode = "black");
 
-function disableBlackMode() {
-    blackModeEnabled = false;
-    squares.forEach(square => {
-        square.removeAttribute("style");
-    });
-};
+rainbowButton.addEventListener("click", event => currentMode = "rainbow");
 
-function enableRainbowMode() {
-    rainbowModeEnabled = true;
-    squares.forEach(square => {
-        let passCount = 0;
-        square.addEventListener("mouseover", event => {
-            if (rainbowModeEnabled) {
-                const red = Math.floor(Math.random() * 256);
-                const blue = Math.floor(Math.random() * 256);
-                const green = Math.floor(Math.random() * 256);
-
-                square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-            };
-        });
-    });
-};
-
-rainbowButton.addEventListener("click", event => {
-    enableRainbowMode();
-});
-
-function disableRainbowMode() {
-    rainbowModeEnabled = false;
-    squares.forEach(square => {
-        square.removeAttribute("style");
-    });
-};
-
-function enableEraser() {
-    squares.forEach(square => {
-        square.addEventListener("mouseover", event => {
-            square.removeAttribute("style");
-        });
-    });
-};
-
-eraserButton.addEventListener("click", event => {
-    enableEraser();
-});
+eraserButton.addEventListener ("click", event => currentMode = "eraser");
 
 resetButton.addEventListener("click", event => {
-    disableBlackMode();
-    disableRainbowMode();
+    currentMode = "default";
     createGrid(16);
-    inputMessage.textContent = "Enter grid size between 2 and 100";
+    inputMessage.textContent = "Current grid size: 16 x 16 \nEnter number between 2 and 100 to change grid size. ";
     inputBox.value = "";
 });
 
 inputBox.addEventListener("input", event => {
     let size = inputBox.value;
     if (size >= 2 && size <=100) {
-        inputMessage.textContent = `Current grid size: ${size} x ${size}`
-        disableBlackMode();
-        disableRainbowMode();
+        inputMessage.textContent = `\u00A0\nCurrent grid size: ${size} x ${size}`
         createGrid(size);
     } else {
-        inputMessage.textContent = "Invalid input. Enter a number between 2 and 100.\nGrid size has been set to default (16x16)."
+        inputMessage.textContent = "Invalid input.\nGrid size has been set to default (16x16)."
         createGrid(16);
     };
 });
+
+function handleDraw(event) {
+    if (currentMode === "black") {
+        event.target.style.backgroundColor = "black";
+    } else if (currentMode === "rainbow") {
+        const red = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    } else if (currentMode === "eraser") {
+        event.target.removeAttribute("style");
+    }
+}
